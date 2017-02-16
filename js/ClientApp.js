a172fe380553e56020eed81081f71a23e161bfe0
fonzilla <j.alfonso.antonio@gmail.com>
2017-02-16 16:57:35 -0500

    changes to start branch

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,18 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import MyTitle from './MyTitle'
 
-var div = React.DOM.div
-
-var MyTitleFact = React.createFactory(MyTitle)
-
 var MyFirstComponent = React.createClass({
   render: function () {
     return (
-      div(null,
-        MyTitleFact({title: 'props are the best', color: 'peru'}),
-        MyTitleFact({title: 'whatever whatever', color: 'mediumaquamarine'}),
-        MyTitleFact({title: 'out of ideas to type', color: 'tomato'})
-      )
+      <div>
+        <MyTitle title='props are cool' color='rebeccapurple' />
+        <MyTitle title='semicolons are useless' color='mediumaquamarine' />
+        <MyTitle title='whatever whatever' color='peru' />
+      </div>
     )
   }
 })
